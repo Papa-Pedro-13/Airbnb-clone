78ef6a49a09c6bae776a5e78aa1ffe7845d08518
Papa-Pedro-13 <aleksandrostrovsqi@gmail.com>
2023-07-24 11:11:24 +0300

    Listing step2(part2, Map)

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -2,12 +2,14 @@
 
 import useRentModal from "@/app/hooks/useRentModal";
 import Modal from "./Modal";
+
 import { useMemo, useState } from "react";
 import Heading from "../Heading";
 import { categories } from "../navbar/Categories";
 import CategoryInput from "../inputs/CategoryInput";
 import { FieldValue, FieldValues, useForm } from "react-hook-form";
 import CountrySelect from "../inputs/CountrySelect";
+import dynamic from "next/dynamic";
 
 enum STEPS {
     CATEGORY = 0,
@@ -45,6 +47,15 @@ const RentModal = () => {
     });
 
     const category = watch("category");
+    const location = watch("location");
+
+    const Map = useMemo(
+        () =>
+            dynamic(() => import("../Map"), {
+                ssr: false,
+            }),
+        [location]
+    );
 
     const setCustomValue = (id: string, value: any) => {
         setValue(id, value, {
@@ -109,10 +120,12 @@ const RentModal = () => {
             <div className="flex flex-col gap-8">
                 <Heading title="Where is your place?" subtitle="Help guests" />
                 <CountrySelect
-                    onChange={() => {
-                        console.log("sds");
+                    value={location}
+                    onChange={(value) => {
+                        setCustomValue("location", value);
                     }}
                 />
+                <Map center={location?.latlng} />
             </div>
         );
     }
